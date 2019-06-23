d85b749d79a016a19df7f75570e0012a83655033
Thomas Mansencal <thomas.mansencal@gmail.com>
2019-06-23 20:36:18 +1200

    Update various examples.

--- colour_datasets/examples/examples_load.py
+++ colour_datasets/examples/examples_load.py
@@ -12,7 +12,7 @@ message_box('Listing the available datasets.\n\n'
             'has been synced locally.')
 print(colour_datasets.datasets())
 
-message_box('A dataset is loaded by using its unique number: "295257"')
+message_box('A dataset is loaded by using its unique number: "3245895"')
 print(colour_datasets.load('3245895'))
 
 message_box('Or alternatively its full title: "New Color Specifications '

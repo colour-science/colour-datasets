90c839937cadc407b0d858afa97f3c0e9f23879f
Thomas Mansencal <thomas.mansencal@gmail.com>
2019-10-07 19:59:42 +1300

    Enable "pre-commit".

--- utilities/export_todo.py
+++ utilities/export_todo.py
@@ -32,9 +32,12 @@ About
 -----
 
 | **Colour - Datasets** by Colour Developers
-| Copyright © 2019 – Colour Developers – `colour-science@googlegroups.com <colour-science@googlegroups.com>`_
-| This software is released under terms of New BSD License: https://opensource.org/licenses/BSD-3-Clause
-| `https://github.com/colour-science/colour-datasets <https://github.com/colour-science/colour-datasets>`_
+| Copyright © 2019 – Colour Developers – \
+`colour-science@googlegroups.com <colour-science@googlegroups.com>`_
+| This software is released under terms of New BSD License: \
+https://opensource.org/licenses/BSD-3-Clause
+| `https://github.com/colour-science/colour-datasets \
+<https://github.com/colour-science/colour-datasets>`_
 """ [1:]
 
 

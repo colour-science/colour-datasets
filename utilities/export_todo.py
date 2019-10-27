8fd870250da3ee735b0b83a14078b0fe3c5b4a69
Thomas Mansencal <thomas.mansencal@gmail.com>
2019-10-27 21:33:31 +1300

    Update "export_todo.py" utility.

--- utilities/export_todo.py
+++ utilities/export_todo.py
@@ -33,11 +33,11 @@ About
 
 | **Colour - Datasets** by Colour Developers
 | Copyright © 2019 – Colour Developers – \
-`colour-science@googlegroups.com <colour-science@googlegroups.com>`_
+`colour-science@googlegroups.com <colour-science@googlegroups.com>`__
 | This software is released under terms of New BSD License: \
 https://opensource.org/licenses/BSD-3-Clause
 | `https://github.com/colour-science/colour-datasets \
-<https://github.com/colour-science/colour-datasets>`_
+<https://github.com/colour-science/colour-datasets>`__
 """ [1:]
 
 

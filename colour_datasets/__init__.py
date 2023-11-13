e9a94cb19f6de4fdcc67a33eec4f06664c9373bc
Thomas Mansencal <thomas.mansencal@gmail.com>
2023-11-14 07:02:49 +1300

    Raise package version to 0.2.4.

--- colour_datasets/__init__.py
+++ colour_datasets/__init__.py
@@ -51,7 +51,7 @@ __application_name__ = "Colour - Datasets"
 
 __major_version__ = "0"
 __minor_version__ = "2"
-__change_version__ = "3"
+__change_version__ = "4"
 __version__ = ".".join(
     (__major_version__, __minor_version__, __change_version__)
 )

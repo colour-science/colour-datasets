colour-science/colour-clf-io

Fix incorrect package reference in `unicode_to_ascii.py` file.

utilities/unicode_to_ascii.py
@@ -71,4 +71,4 @@ def unicode_to_ascii(root_directory: str) -> None:
 if __name__ == "__main__":
     os.chdir(os.path.dirname(__file__))
 
-    unicode_to_ascii(os.path.join("..", "colour"))
+    unicode_to_ascii(os.path.join("..", "colour_clf_io"))

colour-science/colour-datasets

utilities/unicode_to_ascii.py
@@ -68,4 +68,4 @@ def unicode_to_ascii(root_directory: str) -> None:
 if __name__ == "__main__":
     os.chdir(os.path.dirname(__file__))
 
-    unicode_to_ascii(os.path.join("..", "colour_checker_detection"))
+    unicode_to_ascii(os.path.join("..", "colour_datasets"))
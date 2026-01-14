c553f53a721cc9c9a2c67ff582dbe10c800eda2f
Thomas Mansencal <thomas.mansencal@gmail.com>
2026-01-14 20:43:18 +1300

    Remove legacy *NumPy* 1.13 print options.

--- colour_datasets/__init__.py
+++ colour_datasets/__init__.py
@@ -21,12 +21,10 @@ Subpackages
 
 from __future__ import annotations
 
-import contextlib
 import os
 import subprocess
 
 import colour
-import numpy as np
 
 # isort: split
 
@@ -77,7 +75,3 @@ except Exception:  # noqa: BLE001
 colour.utilities.ANCILLARY_COLOUR_SCIENCE_PACKAGES["colour-datasets"] = _version  # pyright: ignore
 
 del _version
-
-# TODO: Remove legacy printing support when deemed appropriate.
-with contextlib.suppress(TypeError):
-    np.set_printoptions(legacy="1.13")
